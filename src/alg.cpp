302a4d1efd955034ca70a57fe11fdaad27b522af
abramovayulia <116936584+abramovayulia@users.noreply.github.com>
2023-03-25 23:58:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,52 +1,54 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len-1; i++) {
-    for (int j = i+1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count += 1;
-      }
-    }
-  }
-   return count;
+    int kol = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i+1; j < len; j++) {
+            if (arr[i] + arr[j] == value)
+                kol++;
+            }
+        }
+    return kol;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = len - 1; i < j; j--) {
-      if (arr[i] + arr[j] == value) {
-        count += 1;
-      }
+    int kol = 0;
+    len--;
+    while (arr[len] > value) {
+        len--;
     }
-  }
-  return count;
+    for (int i = 0; i < len; i++) {
+        for (int j = len; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                kol++;
+            }
+        }
+    }
+    return kol;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-     int left = i, right = len;
-     while (left < right - 1) {
-       int mid = (left + right) / 2;
-       if (arr[i] + arr[mid] == value) {
-        count += 1;
-        int newmid = mid + 1;
-        while (arr[i] + arr[newmid] == value) {
-          count += 1;
-          newmid += 1;
+    int kol = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int lev = i, prav = len;
+        while (lev + 1 < prav) {
+            int mid = (lev + prav) / 2;
+            if (arr[i] + arr[mid] > value) {
+                prav = mid;
+            } else if (arr[i] + arr[mid] < value) {
+                lev = mid;
+            } else {
+                kol++;
+                int a = mid + 1;
+                while (arr[i] + arr[a] == value && a < prav) {
+                    kol++;
+                    a++;
+                }
+                a = mid - 1;
+                while (arr[i] + arr[a] == value && a > lev) {
+                    kol++;
+                    a--;
+                }
+                break;
+            }
         }
-        newmid = mid - 1;
-        while (arr[i] + arr[newmid] == value) {
-          count += 1;
-          newmid -= 1;
-        }
-        break;
-      } 
-      if (arr[i] + arr[mid] > value) {
-        right = mid;
-      } else {
-        left = mid;
-      }
     }
-  }
-  return count;
+    return kol;
 }
